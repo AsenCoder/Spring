963cd4e4b92f23d3d5899e476fe75ffd46a0f4d3
AsenCoder <houasen@163.com>
2016-09-21 22:35:06 +0800

    新增了一个文件cat

---
+++ spring_mybatis_001/src/com/atguigu/mybatis/entities/Cat.java
@@ -0,0 +1,14 @@
+package com.atguigu.mybatis.entities;
+
+public class Cat {
+
+	private String name;
+	
+	private int age;
+	
+	
+	public void run(){
+		
+		System.out.println("faster...!");
+	}
+}
